9cf18d05369f03b905d2fc45a823891929c9cb80
Luis Matos <luismatos138@gmail.com>
2023-05-22 15:54:30 +0100

    added a cart

--- ChatGPTwebApp/src/App.jsx
+++ ChatGPTwebApp/src/App.jsx
@@ -2,6 +2,33 @@ import React from "react";
 import "./style.css";
 
 function App() {
+  const [cartItems, setCartItems] = React.useState([]);
+  const [isCartOpen, setCartOpen] = React.useState(false);
+
+  // Function to add a product to the cart
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+    setCartOpen(true); // Open the cart panel when adding items
+  };
+
+  // Function to remove a product from the cart
+  const removeFromCart = (product) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== product.id);
+    setCartItems(updatedCartItems);
+  };
+
+  // Function to handle the "Encomendar" button click
+  const handleOrder = () => {
+    // Send email with the cart items
+    // Implement your email sending logic here
+    const emailContent = cartItems.map((item) => `${item.name}: ${item.description}`).join('\n');
+    console.log('Email content:', emailContent);
+
+    // Clear the cart after placing the order
+    setCartItems([]);
+    setCartOpen(false); // Close the cart panel after placing the order
+  };
+
   const products = [
     {
       id: 1,
@@ -33,13 +60,35 @@ function App() {
             <li key={product.id} className="card">
               <h3>{product.name}</h3>
               <p>{product.description}</p>
+              <button onClick={() => addToCart(product)}>Add to Cart</button>
             </li>
           ))}
         </ul>
       </main>
+
+      {isCartOpen && (
+        <div className="cart-panel">
+          <h2>Cart</h2>
+          {cartItems.length === 0 ? (
+            <p>Your cart is empty.</p>
+          ) : (
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.id}>
+                  <h3>{item.name}</h3>
+                  <p>{item.description}</p>
+                  <button onClick={() => removeFromCart(item)}>Remove</button>
+                </li>
+              ))}
+            </ul>
+          )}
+          <button onClick={handleOrder} disabled={cartItems.length === 0}>
+            Encomendar
+          </button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
