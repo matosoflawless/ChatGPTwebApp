7b936ffc3546f49c6e4e7253d1ff3f9b9641cbdd
Luis Matos <luismatos138@gmail.com>
2023-05-22 14:49:55 +0100

    First steps

--- ChatGPTwebApp/src/App.jsx
+++ ChatGPTwebApp/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./style.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    {
+      id: 1,
+      name: "Rissóis de Carne",
+      description: "Rissóis recheados com carne",
+    },
+    {
+      id: 2,
+      name: "Rissóis de Camarão",
+      description: "Rissóis recheados com 2 camarões",
+    },
+    {
+      id: 3,
+      name: "Rissóis de pizza",
+      description: "Rissóis rechados com todo o sabor de uma pizza tipica Portuguesa",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header>
+        <img src="src\images\LOGO SAL E AÇUCAR (1).png" alt="Big Image" className="responsive-image" />
+      </header>
+
+      <main>
+      <h2 className="list-heading">Lista de produtos</h2>
+        <ul>
+          
+  {products.map((product) => (
+    <li key={product.id} className="list-item">
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+    </li>
+  ))}
+</ul>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
